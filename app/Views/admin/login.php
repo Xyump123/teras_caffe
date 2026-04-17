e9f1942ae234a1fd2fd67a15ae606484ee415c16
Fauzan Fachrul Rozy <www.fauzanfr@gmail.com>
2026-04-18 00:26:30 +0700

    pesan perubahan

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -274,11 +274,6 @@
                 <i class="fa fa-sign-in-alt"></i> Login
             </button>
 
-            <div class="link">
-                Belum punya akun?
-                <a href="<?= base_url('admin/register') ?>">Register</a>
-            </div>
-
         </form>
 
         <div class="footer">
